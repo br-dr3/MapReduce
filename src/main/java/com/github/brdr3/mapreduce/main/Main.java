71b6beaf201025e0e9f8227b4e2e4453ccf6e452
theo <lucastheo.g.a@gmail.com>
2019-08-20 21:16:02 -0300

    Versao alfa.0

--- src/main/java/com/github/brdr3/mapreduce/main/Main.java
+++ src/main/java/com/github/brdr3/mapreduce/main/Main.java
@@ -11,28 +11,28 @@ public class Main {
     static Logger logger = Logger.getLogger("log4j.properties");
     public static void main(String[] args) {
 
-        //if (System.getProperty("coordinator") != null) {
-        if ( args[0].equals("coordinator") ) {
-            //boolean b = Boolean.parseBoolean(System.getProperty("coordinator"));
-            //if(b) {
-                int mappers = Integer.parseInt(args[1]);
+        if (System.getProperty("coordinator") != null) {
+        //if ( args[0].equals("coordinator") ) {
+            boolean b = Boolean.parseBoolean(System.getProperty("coordinator"));
+            if(b) {
+                int mappers = Integer.parseInt(System.getProperty("mapper"));
                 CoordinatorServer cs = new CoordinatorServer(mappers);
                 cs.start();
-            //}
-        } //else if(System.getProperty("reducer") != null) {
-        else if(args[0].equals("reducer") ) {
-            //boolean b = Boolean.parseBoolean(System.getProperty("reducer"));
-            //if(b) {
+            }
+        } else if(System.getProperty("reducer") != null) {
+        //else if(args[0].equals("reducer") ) {
+            boolean b = Boolean.parseBoolean(System.getProperty("reducer"));
+            if(b) {
                 ReducerServer rs = new ReducerServer();
                 rs.start();
-            //}
-        }// else if(System.getProperty("client") != null) {
-        else if(args[0].equals("client")) {
-            //boolean b = Boolean.parseBoolean(System.getProperty("client"));
-            //if(b) {
+            }
+        } else if(System.getProperty("client") != null) {
+        //else if(args[0].equals("client")) {
+            boolean b = Boolean.parseBoolean(System.getProperty("client"));
+            if(b) {
                 Client c = new Client();
                 c.start();
-            //}
+            }
         }
         else 
             throw new RuntimeException("Profile not specified");
